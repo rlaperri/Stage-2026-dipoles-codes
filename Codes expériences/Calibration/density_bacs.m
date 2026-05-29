92677436d5af6e144b6538f769ce28d74d5ddd9f
rlaperri <robin.laperriere@ens-lyon.fr>
2026-05-29 10:22:37 +0200

    Commentage density_bacs

---
+++ Codes expériences/Calibration/density_bacs.m
@@ -0,0 +1,50 @@
+% Written by Christophe BROUZET
+% Additional comments by Robin LAPERRIÈRE the 29/05/2026
+
+function [y] = density_bacs( N , H , L , W , V_lost , position)
+
+%{
+DESCRIPTION:
+Given the parameters of the tank where we want to create a vertical
+and stable stratification using salt, this function returns the two 
+densities that one should use in order to fill the tank using the two
+buckets methods.
+%} 
+
+% N is the Brunt-Vaisala pulsation that we want
+% H (cm) is the height of fluid in the tank
+% L (cm) is the length of the tank
+% W (cm) is the width of the tank
+% V_lost is the volume that remains below the propeller
+% position : describe how we fill the tank: 'bottom' or 'top'
+
+
+rho_water = 998;
+%V_lost = y.h_lost*60*60;  % volume that remains below the propeller
+V_tot = H*L*W;
+r=V_lost/(V_tot/2+V_lost); 
+g=9.81;                   % pesanteur
+s=1/(g/(N*N*H*0.01)+0.5); % coefficient used to calculate the variation in 
+t=1/(g/(N*N*H*0.01)-0.5); % the tank knowing N and H
+%y.h = V_tot/(2*60*60) ;   % height of water that we need in each water tank
+
+
+y.h = V_tot/(2*1000) ;   % height of water that we need in each water tank
+
+switch position
+
+    case 'bottom' 
+        y.rho_tank = rho_water*(1+t/(1-r)); % density in the saltwater tank 
+        y.rho_min= rho_water;
+        y.rho_max= y.rho_tank*(1-r)+rho_water*r;
+    
+    
+    case 'top'
+        y.rho_tank=rho_water*(1-r)/(1-r-s); 
+        y.rho_min= rho_water*(1-r)+y.rho_tank*r;
+        y.rho_max= y.rho_tank;
+     
+end
+
+% density_bacs(1.5,38,79,33,0,'bottom')
+% CHANGER V_lost et remesurer 
