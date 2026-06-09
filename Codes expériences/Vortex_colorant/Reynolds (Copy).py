1e51d84961a15a8723b3ee4cc79c5fdc1500d027
rlaperri <robin.laperriere@ens-lyon.fr>
2026-06-09 11:57:19 +0200

    Premier README succinct

---
+++ Codes expériences/Vortex_colorant/Reynolds (Copy).py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun  4 08:39:50 2026
+
+@author: Robin Laperrière
+
+À partir des paramètres d'injection, calcule le nombre de Reynolds du 
+Vortex grâce aux paramètres d'injection et la formule trouvée par 
+Nathan BAGSHAW
+"""
+
+# Librairies importées
+
+import numpy as np
+
+# Paramètres physiques
+
+D = 0.5e-3 # Diamètre de l'aiguille utilisée (m)
+nu = 1e-6 # Viscosité cinématique de l'eau (m2/s, pour 20-25 °C)
+
+def Reynolds_Injection(Q) :
+    
+    '''
+    Calcule le nombre de Reynolds ReI du jet d'injection
+    du vortex pour un débit donné
+    
+    Arguments:
+        - Q : float ou numpy array 
+              débit en mL/min
+    Renvoie : 
+        - ReI : float ou numpy array
+                nombre de Reynolds du jet d'injection
+    '''
+    
+    Q_SI = Q/(1e6*60) # en m3/s
+    ReI = (4*Q_SI)/(np.pi*D*nu)
+    
+    return ReI
+
+def Reynolds_Vortex(Q, dt, t):
+    
+    '''
+    Calcule le nombre de Reynolds du Vortex à une incertitude et une relation
+    de proportionnalité près avec la formule trouvée par Nathan BAGSHAW
+    
+    Arguments : 
+        - Q : float ou numpy array
+              débit en mL/min
+        - dt : float ou numpy array
+               durée d'impulsion en s
+        - t : float ou numpy array
+              temps en s 
+    Renvoie : 
+        - ReV : float ou numpy array
+                le nombre de Reynolds estimé du vortex pour Q, dt, t
+        - [ReV_min, ReV_max] : float ou numpy array
+                               l'intervalle de confiance sur le nombre 
+                               de Reynolds estimé
+    '''
+    
+    ReI = Reynolds_Injection(Q)
+    # Nombre de Reynolds du Vortex
+    ReV = np.power(ReI,3/2)*dt*np.power(t,-1/2) 
+    # Bornes de l'intervalle de confiance
+    ReV_min, ReV_max = np.power(ReI,3/2)*dt*np.power(t,-1/2-0.15), np.power(ReI,3/2)*dt*np.power(t,-1/2+0.15)
+    
+    return ReV, np.array([ReV_min, ReV_max])
+    
---
+++ README.md
@@ -0,0 +1,28 @@
+ Stage-2026-dipoles-codes
+===
+
+Ce dépôt contient tout les codes utilisés dans le cadre de mon stage de L3 à l'INPHYNI à Nice. Le stage s'intéressait à des méthodes d'études expérimentales et numériques de la collision 2D d'un dipôle tourbillonnaire.
+
+# Dépendances 
+ 
+Plus de précision plus tard mais matplotlib, numpy scipy et dedalus project probablement
+
+# Utilisation 
+ 
+## Codes expériences
+
+### Calibrations
+
+Ce dossier contient toutes les calibrations des appareils de mesures.
+
+### Stratification
+
+Ce dossier contient toutes les mesures datées des stratifications réalisées dans la cuve et le code matlab pour les calibrer.
+
+### Vortex_Colorant
+
+Ce dossier contient tout les résultats des observations qualitatives sur le comportement des dipôles lors de la collision dans différentes configurations.
+
+# Auteurs
+
+Ce dépôt et la plupart des codes ont été créés par Robin LAPERRIÈRE étudiant en L3 à l'ENS de Lyon. D'autres codes étaient écris par Christophe BROUZET. Le stage était encadré par Christophe BROUZET et Simon THALABARD.
